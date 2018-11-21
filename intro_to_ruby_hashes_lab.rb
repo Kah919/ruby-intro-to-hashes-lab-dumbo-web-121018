97d5937df2ebbdf22f2bf3706819d130f2160a86
Kah Yap <kahyap13@gmail.com>
2018-11-21 00:43:21 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,8 +18,8 @@ def monopoly_with_second_tier
 	monopoly = {
 		raildroads: {
 			pieces: 4,
-			names: {},
-			rent_in_dollars: {}
+					names: {},
+					rent_in_dollars: {}
 		}
 	}
 	# monopoly = {
